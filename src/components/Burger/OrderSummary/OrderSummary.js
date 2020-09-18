ad096cd017cafdc280701beac2fd1959842e0268
Edgar Dias <edgarus@gmail.com>
2020-09-18 14:46:52 +0100

    refactor to improve performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends Component{
+
+    // Debugging
+    // componentDidUpdate(){
+    //     console.log('[OrderSummary] Did Update');
+    // }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+
         return (
-            <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
-        );
-    });
-    return (
-        <Aux>
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <p><strong>Total Price: {this.props.price}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
+        );
+    }
 };
 
 export default OrderSummary;
